5d0fa4ea8b1ee1dce07bff837ed3b5263906e719
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2020-04-15 14:29:01 -0700

    added check date time booked for trainers

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,17 +1,35 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findSameDateAppointment = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+  if (findSameDateAppointment) {
+    return response
+      .status(400)
+      .json({ message: 'Professional can not be booked at this time' });
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
