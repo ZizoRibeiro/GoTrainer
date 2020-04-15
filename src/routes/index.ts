ff03c8402b16563646e564459eb9b8101e143e49
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2020-04-15 12:30:00 -0700

    added appointment route to routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express';
+import appointmentsRouter from './appointments.routes';
 
 const routes = Router();
 
-routes.get('/', (request, response) =>
-  response.json({ message: 'Server is UP and Running!' }),
-);
+routes.use('/appointments', appointmentsRouter);
 
 export default routes;
