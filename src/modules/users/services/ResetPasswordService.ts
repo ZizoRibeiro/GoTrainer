24365a9cd4eb240d4611d8bc9c5a81971750d221
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2020-06-15 13:06:04 -0700

    added token expiration date to reset password service

--- src/modules/users/services/ResetPasswordService.ts
+++ src/modules/users/services/ResetPasswordService.ts
@@ -1,5 +1,5 @@
 import { inject, injectable } from 'tsyringe';
-
+import { isAfter, addHours } from 'date-fns';
 import AppError from '@shared/errors/AppError';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import IUserTokensRepository from '@modules/users/repositories/fakes/FakeUserTokensRepository';
@@ -35,6 +35,13 @@ class ResetPasswordResetService {
 
     if (!user) {
       throw new AppError('User does not exist!');
+    }
+
+    const tokenCreatedAt = userToken.created_at;
+    const compareDate = addHours(tokenCreatedAt, 2);
+
+    if (isAfter(Date.now(), compareDate)) {
+      throw new AppError('Token is expired!');
     }
 
     user.password = await this.hashProvider.generateHash(password);
