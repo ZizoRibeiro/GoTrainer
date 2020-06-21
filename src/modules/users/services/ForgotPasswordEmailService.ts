4913b63ca2b9e4397181b409536f63804718268c
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2020-06-20 21:57:06 -0700

    added file template email to forgot password email service

--- src/modules/users/services/ForgotPasswordEmailService.ts
+++ src/modules/users/services/ForgotPasswordEmailService.ts
@@ -1,11 +1,10 @@
 import { inject, injectable } from 'tsyringe';
+import path from 'path';
 
 import AppError from '@shared/errors/AppError';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
-
-// import User from '@modules/users/infra/typeorm/entities/User';
 
 interface IRequest {
   email: string;
@@ -35,6 +34,13 @@ class ForgotPasswordEmailService {
 
     const { token } = await this.userTokensRepository.generate(user.id);
 
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    );
+
     await this.mailProvider.sendMail({
       to: {
         name: user.name,
@@ -42,10 +48,10 @@ class ForgotPasswordEmailService {
       },
       subject: '[AWP] Password recovery',
       templateData: {
-        template: 'Hi, {{name}}: {{token}}',
+        file: forgotPasswordTemplate,
         variables: {
           name: user.name,
-          token,
+          link: `http://localhost:3000/reset_password?token=${token}`,
         },
       },
     });
