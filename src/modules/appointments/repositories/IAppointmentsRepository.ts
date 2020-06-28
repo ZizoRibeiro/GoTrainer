b0a17d642233a0d26bb022fb28986a8c91c2480f
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2020-06-28 15:57:32 -0700

    added find month appointment provider

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,7 +1,11 @@
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import IFindMonthAppointmentsProviderDTO from '@modules/appointments/dtos/IFindMonthAppointmentsProviderDTO';
 import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
   findByDate(data: Date): Promise<Appointment | undefined>;
+  findMonthAppointmentsProvider(
+    data: IFindMonthAppointmentsProviderDTO,
+  ): Promise<Appointment[]>;
 }
