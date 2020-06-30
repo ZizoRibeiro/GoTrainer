098ee98b5833aca2f92dda0ce9052f48bfb275a8
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2020-06-29 17:21:17 -0700

    added list provider monthly availability service

--- src/modules/appointments/services/ListProviderMonthlyAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthlyAvailabilityService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import { getDaysInMonth, getDate } from 'date-fns';
 
 import IAppointmentRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 
@@ -24,8 +25,8 @@ class ListProviderMonthlyAvailabilityService {
 
   public async execute({
     provider_id,
-    month,
     year,
+    month,
   }: IRequest): Promise<IResponse> {
     const appointments = await this.appointmentsRepository.findMonthAppointmentsProvider(
       {
@@ -34,9 +35,26 @@ class ListProviderMonthlyAvailabilityService {
         month,
       },
     );
-    console.log(appointments);
 
-    return [{ day: 1, available: false }];
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
+
+    const eachDayArray = Array.from(
+      { length: numberOfDaysInMonth },
+      (_, index) => index + 1,
+    );
+
+    const availability = eachDayArray.map(day => {
+      const appointmentsInDay = appointments.filter(appointment => {
+        return getDate(appointment.date) === day;
+      });
+
+      return {
+        day,
+        available: appointmentsInDay.length < 10,
+      };
+    });
+
+    return availability;
   }
 }
 
