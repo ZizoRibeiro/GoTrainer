c48100f915462502ba79ad9c2da591005c36f45d
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2020-07-01 22:44:55 -0700

    added find daily availability provider method to appointment repo

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,11 +1,15 @@
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import IFindMonthAppointmentsProviderDTO from '@modules/appointments/dtos/IFindMonthAppointmentsProviderDTO';
-import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+import IFindDailyAvailabilityProviderDTO from '@modules/appointments/dtos/IFindDailyAvailabilityProvidderDTO';
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
   findByDate(data: Date): Promise<Appointment | undefined>;
   findMonthAppointmentsProvider(
     data: IFindMonthAppointmentsProviderDTO,
+  ): Promise<Appointment[]>;
+  findDailyAvailabilityProvider(
+    data: IFindDailyAvailabilityProviderDTO,
   ): Promise<Appointment[]>;
 }
