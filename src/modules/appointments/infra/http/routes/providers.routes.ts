e6bd14b38db7a4c4a8d749ca0722c42b3febb141
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2020-07-02 14:22:44 -0700

    added route for provider monthly and daily availability

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,13 +1,27 @@
 import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import ProviderDailyAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderDailyAvailabilityController';
+import ProviderMonthlyAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderMonthlyAvailabilityController';
+
 import ProvidersController from '@modules/appointments/infra/http/controllers/ProvidersController';
 
 const providersRouter = Router();
+
 const providersController = new ProvidersController();
+const providerMonthlyAvailabilityController = new ProviderMonthlyAvailabilityController();
+const providerDailyAvailabilityController = new ProviderDailyAvailabilityController();
 
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', providersController.index);
+providersRouter.get(
+  '/:provider_id/monthly-availability',
+  providerMonthlyAvailabilityController.index,
+);
+providersRouter.get(
+  '/:provider_id/daily-availability',
+  providerDailyAvailabilityController.index,
+);
 
 export default providersRouter;
