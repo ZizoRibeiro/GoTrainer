5cff50b8a90d4b6acd1b8e7bab987024ddc40196
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2020-07-04 13:21:48 -0700

    added profile route validation

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 import ProfileController from '@modules/users/infra/http/controllers/ProfileController';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import { JoinAttribute } from 'typeorm/query-builder/JoinAttribute';
 
 const profileRouter = Router();
 const profileController = new ProfileController();
@@ -10,6 +12,17 @@ const profileController = new ProfileController();
 profileRouter.use(ensureAuthenticated);
 
 profileRouter.get('/', profileController.show);
-profileRouter.put('/', profileController.update);
+profileRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string(),
+      password_confirmation: Joi.string().valid(Joi.ref('password')),
+    },
+  }),
+  profileController.update,
+);
 
 export default profileRouter;
