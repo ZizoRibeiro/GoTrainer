e2dff3cc88a1a4f5ec1b506e52257f1f93de63b4
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2020-07-17 23:00:05 -0700

    added serialization class to class to the object appointment

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -3,6 +3,7 @@ import { injectable, inject } from 'tsyringe';
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import IAppointmentRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import { classToClass } from 'class-transformer';
 
 // import User from '@modules/users/infra/typeorm/entities/User';
 
@@ -45,7 +46,7 @@ class ListProviderAppointmentsService {
         },
       );
 
-      await this.cacheProvider.save(cacheKey, appointments);
+      await this.cacheProvider.save(cacheKey, classToClass(appointments));
     }
 
     return appointments;
